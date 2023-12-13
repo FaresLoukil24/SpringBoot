2c9c2020d5a3186cd53b8f2c81663e48bb3b3467
FaresLoukil <fares.loukil@esprit.tn>
2023-12-13 16:01:39 +0100

    update

--- src/main/java/tn/esprit/faresloukil/foyer_fares_loukil/DAO/Repository/EtudiantRepository.java
+++ src/main/java/tn/esprit/faresloukil/foyer_fares_loukil/DAO/Repository/EtudiantRepository.java
@@ -4,12 +4,34 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.faresloukil.foyer_fares_loukil.DAO.Entities.Etudiant;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface EtudiantRepository extends JpaRepository<Etudiant,Long> {
     Etudiant findByCin(long cin);
-    List<Etudiant> findByNomLike(String nom);
-    List<Etudiant> findByNomContains(String nom);
-    List<Etudiant> findByNomContaining(String nom);
+
+    @Override
+    List<Etudiant> findAll();
+
+    List<Etudiant> findByNomLike();
+    List<Etudiant> findByNomContains();
+    List<Etudiant> findByNomContaining();
+
+    Etudiant findByNomAndPrenom(String nom,String prenom);
+    Etudiant findBycin(long cin);
+
+    List<Etudiant> findByDateNaissanceGreaterThan(LocalDate date);
+
+    List<Etudiant> findByEcole(String ecole);
+
+    List<Etudiant> findByReservationsAndId(long id);
+
+    //6- Recherche des étudiants par réservation (année de réservation)
+    List<Etudiant>findByReservationsAnneUniversitaire(Date anne);
+
+    List<Etudiant> findByNaissanceGreaterThanAndecole(LocalDate date,String nomEcole);
+
+    List<Etudiant> findByReservationsAnneUniversitaireOrderByNaissance(Date anne);
 }
--- src/main/java/tn/esprit/faresloukil/foyer_fares_loukil/DAO/Repository/ReservationRepository.java
+++ src/main/java/tn/esprit/faresloukil/foyer_fares_loukil/DAO/Repository/ReservationRepository.java
@@ -3,6 +3,14 @@ package tn.esprit.faresloukil.foyer_fares_loukil.DAO.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.faresloukil.foyer_fares_loukil.DAO.Entities.Reservation;
 
-public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+import java.util.Date;
+import java.util.List;
 
+public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+    List<Reservation> findReservationByEstValide(boolean Valdie);
+    // Recherche des réservations par l'id de l'étudiant
+    List<Reservation> findByEtudiantsIdEtudiant(long idEtudian);
+    // Recherche des réservations par année et validité
+
+    List<Reservation> findReservationByEstValideAndAnneUniversitaire(boolean Valdie, Date anneeUniversitaire);
 }
